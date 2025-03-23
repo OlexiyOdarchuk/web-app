d088971a1f59df9b240f8c1cc13a8f15883537f8
iShawyha <shawyhaf@gmail.com>
2025-03-23 22:15:43 +0200

    Django lesson 4

--- test/main/views.py
+++ test/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h1>Про нас</h1>")
+    return render(request, 'main/about.html')
