43b75fea2012e55e4fb1e8744408bf30644ca227
iShawyha <shawyhaf@gmail.com>
2025-03-24 22:16:14 +0200

    Django lesson 6

--- test/main/views.py
+++ test/main/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Головна сторінка',
+        'values': ['Some', 'Hello', '12345'],
+        'obj':{
+            'car': 'BMW',
+            'age': '16',
+            'hobby': 'dance'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
